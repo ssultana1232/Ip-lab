8871e5b751ad9a3ddee970a821035b368589754e
ssultana1232 <119288000+ssultana1232@users.noreply.github.com>
2024-01-31 19:45:25 +0600

    Create lab6jsfile.js

---
+++ lab6jsfile.js
@@ -0,0 +1,23 @@
+function hello()
+    
+{
+    let x=5,y=10,z,p;
+    z = x+y;
+
+
+console.log("Hello World");
+console.log("Sum of x and y:" +z);
+console.log(p);
+
+}
+function printMessage(message,times)
+{
+    for(let i=0; i<times;i++)
+    //for(var i=0; i<times;i++)
+    {
+        console.log(message);
+    }
+}
+
+//hello();
+printMessage("Hello World!",10);
